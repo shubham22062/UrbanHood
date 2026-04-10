6334d2bb6c42dc9db1e6d2bd2724aa3a9dec090a
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-04-10 11:36:52 +0530

    cartmodel

--- backend/src/model/cart.model.ts
+++ backend/src/model/cart.model.ts
@@ -8,5 +8,39 @@ interface ICartItem {
 }
 
 export interface ICart extends Document{
-    
+    user:mongoose.Types.ObjectId;
+    items:ICartItem[];
 }
+
+const cartSchema = new Schema<ICart>({
+
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    items:[
+        {
+            product:{
+                type:Schema.Types.ObjectId,
+                ref:"Product",
+                required:true,
+            
+            },
+            quantity:{
+                type:Number,
+                required:true,
+                default:1,
+            },
+            size:{
+                type:String,
+            },
+            color:{
+                type:String,
+            },
+        },
+    ],
+},{timestamps:true})
+
+export const Cart = mongoose.model<ICart>("Cart" , cartSchema);
+

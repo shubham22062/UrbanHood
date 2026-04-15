f40db270083b57155a4dac28acd252e04af41904
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-04-16 00:19:48 +0530

    cart Controller

--- backend/src/controllers/cart.controllers.ts
+++ backend/src/controllers/cart.controllers.ts
@@ -55,3 +55,60 @@ export const getCart = async(req:AuthRequest, res:Response)=>{
         res.status(500).json({message:error.message});
     }
 }
+
+export const updateCart = async(req:AuthRequest, res:Response)=>{
+    try {
+       const{productId, quantity} = req.body;
+       const cart = await Cart.findOne({user:req.user!.id})
+
+       if(!cart){
+        return res.status(404).json({message:"Cart not found"});
+
+       }
+
+       const item = cart.items.find(
+        (item)=>item.product.toString() ===productId
+       );
+
+       if(!item){
+        return res.status(404).json({message:"Item not found"});
+
+       }
+
+       item.quantity = quantity;
+
+       await cart.save();
+
+       res.json({
+        sucess:true,
+        cart,
+       })
+    } catch (error:any) {
+        res.status(500).json({message:error.message});
+    }
+}
+
+export const removeCartItem = async(req:AuthRequest, res:Response)=>{
+    try {
+       const {productId} = req.params;
+       
+       const cart = await Cart.findOne({user:req.user!.id});
+
+       if(!cart){
+        return res.status(404).json({message:"Cart not found"});
+       }
+
+       cart.items = cart.items.filter(
+        (item)=>item.product.toString()!==productId
+       );
+
+       await cart.save()
+
+       res.json({
+        sucess:true,
+        cart,
+       })
+    } catch (error:any) {
+        res.status(500).json({message:error.message})
+    }
+};

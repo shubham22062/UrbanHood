bc95d06d91dc652753b99eccc516f0ec35c2963f
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-04-16 21:49:26 +0530

    orderModel

---
+++ backend/src/model/order.model.ts
@@ -0,0 +1,54 @@
+import mongoose, { Document,Schema } from "mongoose";
+
+interface IOrderItem{
+    product:mongoose.Types.ObjectId;
+    quantity:number;
+    size:string;
+    color:string;
+}
+
+export interface IOrder extends Document{
+    user:mongoose.Types.ObjectId;
+    items:IOrderItem[];
+    totalPrice:number;
+    shippingAddress:string;
+    paymentStatus:string;
+    orderStatus:string;
+}
+
+const orderSchema = new Schema({
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    items:[
+        {
+            product:{
+            type:mongoose.Types.ObjectId,
+            ref:"Product",
+        },
+        quantity:Number,
+        size:String,
+        color:String,
+    },
+    ],
+    totalPrice:{
+        type:Number,
+        required:true,
+    },
+    shippingAddress:{
+        type:String,
+        required:true,
+    },
+    paymentStatus:{
+        type:String,
+        enum:["pending", "paid"],
+        default:"pending",
+    },
+    orderStatus:{
+        type:String,
+        enum:["processing", "shipped", "deliverd"],
+        default:"processing",
+    },
+},{timestamps:true})
